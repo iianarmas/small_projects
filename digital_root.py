a7b4aadfdf1bcfda16237f184ffdc787ae1fae66
illumi <armas.cav@gmail.com>
2021-08-04 19:22:27 +0800

    Gettin digital root or seed number using recursion

---
+++ digital_root.py
@@ -0,0 +1,17 @@
+def digital(n):
+
+    if n >= 0:
+        n = [i for i in str(n)]
+        n = [int(i) for i in n]
+
+        if len(n) > 1:
+            return digital(sum(n))
+        return n[0]
+    return 'Invalid input'
+
+
+user = int(input('Enter number: '))
+
+d_root = digital(user)
+
+print(f'Digital Root is: {d_root}')
